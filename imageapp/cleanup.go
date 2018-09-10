c1c1db2156a40fb13b246a62d9be98600ba67a16
Thomas Peters-Hall <ramius345@gmail.com>
2018-09-09 20:37:02 -0400

    add database prune program for removing images from the database that no longer exist

---
+++ imageapp/cleanup.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gocql/gocql"
+)
+
+type Entry struct {
+	Filepath  string
+	Sha       string
+	Imagedate time.Time
+	Terminate bool
+}
+
+func doesFileExist(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func removeByShaEntry(entry Entry, batch *gocql.Batch) {
+	batch.Query("delete from images_by_sha where sha=?", entry.Sha)
+}
+
+func determineImageDay(imagedate time.Time) time.Time {
+	day := imagedate.Day()
+	month := imagedate.Month()
+	year := imagedate.Year()
+	location := imagedate.Location()
+
+	dayonly := time.Date(year, month, day, 0, 0, 0, 0, location)
+	return dayonly
+}
+
+func removeByDateEntry(entry Entry, batch *gocql.Batch) {
+	day := determineImageDay(entry.Imagedate)
+	fmt.Printf("Doing lookup on %v,%v,%v\n", day, entry.Imagedate, entry.Sha)
+	batch.Query("delete from images_by_date where day=? and imagedate=? and sha=?",
+		day,
+		entry.Imagedate,
+		entry.Sha)
+}
+
+func removeThumbnailForEntry(entry Entry, thumbnailPath string) error {
+	fullPath := thumbnailPath + "/" + entry.Sha + ".jpg"
+	fmt.Printf("Checking if thumbnail path exists for %s\n", fullPath)
+	if doesFileExist(fullPath) {
+		fmt.Printf("Removing thumbnail %s\n", fullPath)
+		return os.Remove(fullPath)
+	} else {
+		fmt.Printf("Thumbnail already gone %s\n", fullPath)
+		return nil
+	}
+}
+
+func scheduleCleanupThread(entryCleanChannel chan Entry, session *gocql.Session, thumbnailPath string) {
+	finished := false
+	for !finished {
+		entry := <-entryCleanChannel
+		if !entry.Terminate {
+			fmt.Printf("Cleaning up entry for %s %s %v\n", entry.Filepath, entry.Sha, entry.Imagedate)
+
+			batch := session.NewBatch(gocql.LoggedBatch)
+			removeByShaEntry(entry, batch)
+			removeByDateEntry(entry, batch)
+			err := session.ExecuteBatch(batch)
+			if err != nil {
+				fmt.Printf("Executing remove batch failed! %v\n", err)
+			}
+
+			err = removeThumbnailForEntry(entry, thumbnailPath)
+			if err != nil {
+				fmt.Printf("Executing remove thumbnail failed: %v\n", err)
+			}
+
+		}
+		finished = entry.Terminate
+	}
+}
+
+func verifyImagesThread(filePathChannel chan Entry, cleanupChannel chan Entry) {
+	finished := false
+	i := 0
+	for !finished {
+		entry := <-filePathChannel
+		if (!entry.Terminate && !doesFileExist(entry.Filepath)) || entry.Terminate {
+			cleanupChannel <- entry
+		}
+
+		i += 1
+		finished = entry.Terminate
+	}
+}
+
+func main() {
+
+	db_url := "winredgrape.pineapple.no-ip.biz"
+	db_port := 30000
+	path_to_images := "/mnt/san/Pictures/Carolyn"
+	path_to_thumbnails := "/mnt/san/thumbnails"
+
+	fmt.Println("Connecting to database")
+	cluster := gocql.NewCluster(db_url)
+	cluster.Port = db_port
+	cluster.Keyspace = "imageapp"
+	cluster.Consistency = gocql.Quorum
+	session, _ := cluster.CreateSession()
+	defer session.Close()
+
+	iter := session.Query("select sha,relpath,filename,imagedate from images_by_sha").Iter()
+
+	filePathChannel := make(chan Entry, 128)
+	cleanUpChannel := make(chan Entry, 128)
+	go verifyImagesThread(filePathChannel, cleanUpChannel)
+	cleanupThreadSession, _ := cluster.CreateSession()
+	go scheduleCleanupThread(cleanUpChannel, cleanupThreadSession, path_to_thumbnails)
+
+	var sha, relpath, filename string
+	var imagedate time.Time
+	i := 0
+	for iter.Scan(&sha, &relpath, &filename, &imagedate) {
+		filePathChannel <- Entry{path_to_images + "/" + relpath + "/" + filename, sha, imagedate, false}
+		i += 1
+	}
+	fmt.Println("Finished scanning database for image entries, signaling")
+	fmt.Printf("Processed %d db entries\n", i)
+	filePathChannel <- Entry{"", "", time.Time{}, false}
+
+	err := iter.Close()
+	if err != nil {
+		fmt.Printf("An error occured querying images by sha %v\n", err)
+	}
+}
