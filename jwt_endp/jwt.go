9b838acf9458caf8a883bce7b85289aa8ed65c23
Thomas Peters-Hall <ramius345@gmail.com>
2018-09-15 12:00:22 -0400

    more cache nonsense

--- jwt_endp/jwt.go
+++ jwt_endp/jwt.go
@@ -95,9 +95,9 @@ func main() {
 		skey := jwtverify.SigningKey{signing_key}
 		if valid, _ := skey.Verify(tokenString); valid {
 			c.SetCookie("auth_token", tokenString, 30000, "", "", false, false)
-			c.Writer.Header().Add("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate")
+			c.Writer.Header().Add("Cache-Control", "max-age=0, no-cache, no-store, must-revalidate, proxy-revalidate")
 			c.Writer.Header().Add("Pragma", "no-cache")
-			c.Writer.Header().Add("Expires", "0")
+			c.Writer.Header().Add("Expires", "Tue, 03 Jul 2001 06:00:00 GMT")
 			c.Redirect(303, "/")
 		} else {
 			c.JSON(401, gin.H{"error": "Unauthorized"})
